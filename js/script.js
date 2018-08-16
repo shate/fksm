f475f8f8a6567f280f71f510d8c4e30d29e3f9b7
shate <shate@ya.ru>
2018-08-16 10:37:47 +0300

    first

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-	
+
+  $(".hot-news-slider").slick() ;
+  $(".slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4
+  }) ;
+  //tabs
+
+  $('ul.tabs').on('click', 'li:not(.current)', function() {
+    $(this).addClass('current').siblings().removeClass('current')
+        .parents('.tab-section').find('div.box').eq($(this).index()).fadeIn(150).siblings('div.box').hide();
+
+  });
+  //select
+  if($('select').hasClass('selectBlock')){
+    $('.selectBlock').sSelect({
+      defaultText : 'Пожалуйста, выберите из списка',
+      containerClass: 'icon-down-open'
+    });
+  }
 })
